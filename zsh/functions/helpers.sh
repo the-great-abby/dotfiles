c070bf0f42aecefebca7215dbf168af2ae1b78df
Abby Malson <github@augustmalson.com>
2024-02-12 00:41:03 -0700

    updates

--- zsh/functions/helpers.sh
+++ zsh/functions/helpers.sh
@@ -1,4 +1,12 @@
-function help_helpers_todo () {
-  echo "hello"
+function help_helpers_todo {
+  echo "0: $0"
+  echo "hello $1"
 }
-
+function kill_port_proc {
+    lsof -i tcp:"$1" | grep LISTEN | awk '{print $2}'
+}
+if [ -f ~/.zsh/zshalias ]; then
+    source ~/.zsh/zshalias
+else
+    print "404: ~/.zsh/zshalias not found."
+fi
