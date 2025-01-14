1a3146491d7f38be5bbf763d897aff194b98cc97
Your Name <you@example.com>
2025-01-13 20:47:07 -0700

    workout notes/updates

--- zsh/functions/aider.sh
+++ zsh/functions/aider.sh
@@ -3,11 +3,23 @@
 function start_aider_openai() {
   # Read the file into a variable             
   # Requires an OPENAI API Key
-  # apiKey=$(<$HOME/code/dotfiles/aider.txt) 
+  apiKey=$(<$HOME/code/dotfiles/aider_openai.txt) 
   OLLAMA_API_BASE=http://host.docker.internal:11434 
   docker run -it --user $(id -u):$(id -g) -e OLLAMA_API_BASE=$OLLAMA_API_BASE --volume $(pwd):/app paulgauthier/aider-full --set-env OLLAMA_API_BASE=$OLLAMA_API_BASE --model ollama/llama3.2
   #--openai-api-key $OPENAI_API_KEY
 }
+function start_aider_anthropic() {
+  # Read the file into a variable             
+  # Requires an OPENAI API Key
+  apiKey=$(<$HOME/code/dotfiles/aider_anthropic.txt) 
+  #echo $apiKey
+  OLLAMA_API_BASE=http://host.docker.internal:11434 
+  #test="docker run -it --user $(id -u):$(id -g) -e OLLAMA_API_BASE=$OLLAMA_API_BASE --volume $(pwd):/app paulgauthier/aider-full --set-env OLLAMA_API_BASE=$OLLAMA_API_BASE --model ollama/llama3.2 --anthropic-api-key $apiKey"
+  docker run -it --user $(id -u):$(id -g) -e OLLAMA_API_BASE=$OLLAMA_API_BASE --volume $(pwd):/app paulgauthier/aider-full --set-env OLLAMA_API_BASE=$OLLAMA_API_BASE --model ollama/llama3.2 --anthropic-api-key $apiKey
+  #echo $test
+  # `$test`
+}
+
 
 function start_aider_ollama() {
   # Read the file into a variable             
