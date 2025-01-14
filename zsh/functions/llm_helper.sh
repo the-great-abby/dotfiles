2e3f18d2a19cd0fcf3c8ecc2e846b92677a23be6
Your Name <you@example.com>
2025-01-14 08:57:15 -0700

    attempts to move to functions

--- zsh/functions/llm_helper.sh
+++ zsh/functions/llm_helper.sh
@@ -12,11 +12,14 @@ function ask_llm_self_help_related_question() {
       echo "question: $1"
       question=$1
       collection_name="self_help"
+      local character=$(pick_a_character)
       llm_prompt=$(
 cat << END
-You are a self-help guru. Please use the context available as well as
-your existing knowledge to help respond to the question. Please answer
-the question.
+Hi, please announce who you are, you are '$character', and Good Day to you, my name is Abby, and I'd like you to answer the following question.
+
+Playing as a self-help guru. Please use the context available as well as your existing knowledge to help respond to the question. 
+
+
 
 Question:
 --
@@ -34,20 +37,21 @@ END
 END
         )
        #echo "json: $json"
-      response=$(curl -H "Content-Type: application/json" -H "" -X POST -d $json http://localhost:7500/shell_chat)
-      echo $response
+      response=$(generateCurlRequest -json $json)
+      echo "${response}"
       #return $response
 }
 function ask_llm_finance_related_question() {
       echo "0: $0"
       echo "question: $1"
       question=$1
+      local character=$(pick_a_character)
       collection_name="finance"
       llm_prompt=$(
 cat << END
-You are a finance guru, and I need help with a question.
 
-Please answer the question
+Hi, please announce who you are, you are '$character', and Good Day to you, my name is Abby, and I'd like you to answer the following question.
+Roleplaying as a finance guru, and I need help with a question.
 
 Question:
 --
@@ -64,9 +68,9 @@ END
 {"msg": "$htmlEscapeContentValue", "collection": "$collection_name"}
 END
         )
-       #echo "json: $json"
-      response=$(curl -H "Content-Type: application/json" -H "" -X POST -d $json http://localhost:7500/shell_chat)
-      echo $response
+      #echo "json: $json"
+      response=$(generateCurlRequest -json $json)
+      echo "${response}"
       #return $response
 }
 
@@ -74,13 +78,15 @@ END
 function ask_llm_programming_related_question() {
       echo "0: $0"
       echo "question: $1"
+      local character=$(pick_a_character)
       question=$1
       collection_name="programming_assistance"
       llm_prompt=$(
 cat << END
-You are a programming superstar ...
 
-Please answer the question
+Hi, please announce who you are, you are '$character', and Good Day to you, my name is Abby, and I'd like you to answer the following question.
+
+Roleplaying as a programming superstar.
 
 Question:
 --
@@ -98,8 +104,8 @@ END
 END
         )
        #echo "json: $json"
-      response=$(curl -H "Content-Type: application/json" -H "" -X POST -d $json http://localhost:7500/shell_chat)
-      echo $response
+      response=$(generateCurlRequest -json $json)
+      echo "${response}"
       #return $response
 }
 
@@ -107,10 +113,11 @@ function ask_llm_question() {
       echo "0: $0"
       echo "question: $1"
       question=$1
+      local character=$(pick_a_character)
       collection_name="storytelling_with_data"
       llm_prompt=$(
 cat << END
-Please answer the question
+Hi, please announce who you are, you are '$character', and Good Day to you, my name is Abby, and I'd like you to answer the following question.
 
 Question:
 --
@@ -127,28 +134,42 @@ END
 {"msg": "$htmlEscapeContentValue", "collection": "$collection_name"}
 END
         )
-       #echo "json: $json"
-      response=$(curl -H "Content-Type: application/json" -H "" -X POST -d $json http://localhost:7500/shell_chat)
-      echo $response
+      #echo "json: $json"
+      response=$(generateCurlRequest -json $json)
+      echo "${response}"
       #return $response
 }
 
-#function pick_a_character() {
-#        motivational_speakers=("david goggins" "tony horton" "Gordan Ramsey" "Darth Vader")
-#        character="${motivational_speakers[$RANDOM % ${#motivational_speakers[@]}]}"
-#        return character
-#}
-
+function pick_a_character() {
+        local motivational_speakers=("david goggins, a fitness guru" "tony horton, a p90x fitness guru" "Dolly Parton" "Darth Vader"  "Kermit The Frog" "Spiderman" "Wonder Woman" )
+        # local motivational_speakers=("david goggins" "tony horton" "Gordan Ramsey" "Darth Vader")
+        local character="${motivational_speakers[$RANDOM % ${#motivational_speakers[@]}]}"
+        return "${character}"
+}
+function getDailyJournal() {
+        # date
+        echo "Parameter: $-n"  # date
+        local file="$SECOND_BRAIN"'/daily_notes/'${date_of_file}'.md'
+        local content=""
+        cat $file | while read line
+        do
+          content="$content\n$line"
+          #echo "a line: $line"
+        done
+        return "${content}"
+}
 function generateLLMYesterdaysEveningSummary() {
         today=$(date +"%Y-%m-%d")
         yesterday=$(date -v -1d +"%Y-%m-%d")
         day_before_yesterday=$(date -v -2d +"%Y-%m-%d")
 
-        collection_name="storytelling_with_data"
-        file="$SECOND_BRAIN"'/daily_notes/'$(date +"%Y-%m-%d").md
+        collection_name="self_help"
+        #file="$SECOND_BRAIN"'/daily_notes/'$(date +"%Y-%m-%d").md
+        local date_of_file=$(date +"%Y-%m-%d")
         motivational_speakers=("david goggins" "tony horton" "Gordan Ramsey" "Darth Vader")
         
         character="${motivational_speakers[$RANDOM % ${#motivational_speakers[@]}]}"
+        #local character=$(pick_a_character)
         workoutfile="$SECOND_BRAIN"'/04-areas/fitness/workout-'"$yesterday".md
         chorefile="$SECOND_BRAIN"'/chores/chores_day-'"$yesterday".md
         #echo "filename: $file"
@@ -176,7 +197,9 @@ function generateLLMYesterdaysEveningSummary() {
         #echo "Content goes here: $content"
         contentvalue=$(
 cat << END
-Hi, please announce who you are, you are '$character', and Good Day to you, can you help summarize my current dairy entry and congratulate me to checking items off the todo list for the day.
+Hi My name is Abby, and I need your assistance.
+
+Do me a favor and please announce who you are, you are '$character', and Good Day to you, can you help summarize my current dairy entry and congratulate me to checking items off the todo list for the day.
 
 Please assist me in generating a list of items to move the tasks from today's dairy, to move them to tomorrow's diary, please mark this section as "Next Day"
 
@@ -218,16 +241,17 @@ END
 END
         )
         #echo "json: $json"
-        response=$(curl -H "Content-Type: application/json" -H "" -X POST -d $json http://localhost:7500/shell_chat)
+        response=$(generateCurlRequest -json $json)
         echo "${response}"
 }
 
 function generateLLMEveningSummary() {
-        collection_name="storytelling_with_data"
+        collection_name="self_help"
         file="$SECOND_BRAIN"'/daily_notes/'$(date +"%Y-%m-%d").md
         motivational_speakers=("david goggins" "tony horton" "Gordan Ramsey" "Darth Vader")
         
-        character="${motivational_speakers[$RANDOM % ${#motivational_speakers[@]}]}"
+        #character="${motivational_speakers[$RANDOM % ${#motivational_speakers[@]}]}"
+        character=$(pick_a_character)
         workoutfile="$SECOND_BRAIN"'/04-areas/fitness/workout-'"$today".md
         chorefile="$SECOND_BRAIN"'/chores/chores_day-'"$today".md
         #echo "filename: $file"
@@ -255,7 +279,9 @@ function generateLLMEveningSummary() {
         #echo "Content goes here: $content"
         contentvalue=$(
 cat << END
-Hi, please announce who you are, you are '$character', and Good Day to you, can you help summarize my current dairy entry and congratulate me to checking items off the todo list for the day.
+Hi My name is Abby, and I need your assistance.
+
+Do me a favor and please announce who you are, you are '$character', and Good Day to you, can you help summarize my current dairy entry and congratulate me to checking items off the todo list for the day.
 
 Please assist me in generating a list of items to move the tasks from today's dairy, to move them to tomorrow's diary, please mark this section as "Next Day"
 
@@ -297,14 +323,18 @@ END
 END
         )
         #echo "json: $json"
-        response=$(curl -H "Content-Type: application/json" -H "" -X POST -d $json http://localhost:7500/shell_chat)
+        #response=$(curl -H "Content-Type: application/json" -H "" -X POST -d $json http://localhost:7500/shell_chat)
+        response=$(generateCurlRequest -json $json)
         echo "${response}"
 }
 
 function generateCurlRequest () {
         # using the other functions in zsh
         # create a curl call
-        # AI!
+        #echo "Parameter: $-n"  # json
+        # json=${json}
+        local response=$(curl -H "Content-Type: application/json" -H "" -X POST -d $json http://localhost:7500/shell_chat)
+        echo "${response}"
 }
 function generateLLMReviewOfJournalInThePastWeek() {
         # collection_name should be self_help
@@ -317,12 +347,10 @@ function generateLLMReviewOfJournalInThePastWeek() {
 }
 
 function generateLLMNotesSummary() {
-        collection_name="storytelling_with_data"
+        collection_name="self_help"
         file="$SECOND_BRAIN"'/daily_notes/'$(date +"%Y-%m-%d").md
         
-        motivational_speakers=("david goggins, a fitness guru" "tony horton, a p90x fitness guru" "Dolly Parton" "Anakin Skywalker, aka Darth Vader"  "Kermit The Frog" "Spiderman" "Wonder Woman" "Saitama, aka One Punch Man")
         # "Yoda" "Han Solo" "Fred Rogers"
-        motivational_speakers=("david goggins" "tony horton" "Gordan Ramsey" "Darth Vader")
         workoutfile="$SECOND_BRAIN"'/04-areas/fitness/workout-'"$today".md
         chorefile="$SECOND_BRAIN"'/chores/chores_day-'"$today".md
         #echo "filename: $file"
@@ -346,7 +374,8 @@ function generateLLMNotesSummary() {
           #echo "a line: $workoutline"
         done
 
-        character="${motivational_speakers[$RANDOM % ${#motivational_speakers[@]}]}"
+        #character="${motivational_speakers[$RANDOM % ${#motivational_speakers[@]}]}"
+        character=$(pick_a_character)
         echo "Chat with the LLM for Greeting"
         #echo "Content goes here: $content"
         contentvalue=$(
@@ -396,6 +425,6 @@ END
 END
         )
         #echo "json: $json"
-        response=$(curl -H "Content-Type: application/json" -H "" -X POST -d $json http://localhost:7500/shell_chat)
+        response=$(generateCurlRequest -json $json)
         echo "${response}"
 }
